b04df57a1c62d764009f29b919d5eba66502a3ba
EricSchles <ericschles@gmail.com>
2014-12-10 08:50:56 -0800

    fixed file endswith bug

--- counter.py
+++ counter.py
@@ -7,11 +7,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument('extension',nargs=1)
 parser.add_argument('roots',nargs='*')
 args = parser.parse_args()
-extension = args.extension
+extension = args.extension[0]
 roots = args.roots
 
+total_len = 0
+for root in roots:
+    for rootdir,dir,files in os.walk(root):
+        for file in files:
+            if file.endswith(extension):
+                with open(file,"r") as f:
+                    total_len += len(f)
 
-# for root in roots:
-#     for rootdir,dir,files in os.walk(root):
-#         print files
+                
         
+                
