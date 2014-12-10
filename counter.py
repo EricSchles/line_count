f2130c00826effde345a3477c8e3e2d8d94a5034
EricSchles <ericschles@gmail.com>
2014-12-10 10:27:44 -0800

    adding translation

--- counter.py
+++ counter.py
@@ -65,8 +65,24 @@ for root in roots:
 
 print "You worked best between %s and %s so far" % (str(ave_hour/num_files),str((ave_hour/num_files) + 1))
 print "You've written %d lines so far." % total_len
+
     
-month_translate            
+month_translate = {
+    "01":"january",
+    "02":"february",
+    "03":"march",
+    "04":"april",
+    "05":"may",
+    "06":"june",
+    "07":"july",
+    "08":"august",
+    "09":"september",
+    "10":"october",
+    "11":"november",
+    "12":"december"
+}
+ 
+            
 for month in month_totals:
     
     if month_totals[month].num_files != 0:
