1fc31749ac0b6c205d30f1e3202a988e42137b66
EricSchles <ericschles@gmail.com>
2014-12-10 08:39:29 -0800

    counter updated

--- counter.py
+++ counter.py
@@ -4,12 +4,14 @@ import argparse
 #fucking awesome docs - https://mkaz.com/2014/07/26/python-argparse-cookbook/
 
 parser = argparse.ArgumentParser()
+parser.add_argument('extension',nargs=1)
 parser.add_argument('roots',nargs='*')
 args = parser.parse_args()
-roots = args.roots.split(' ')
+extension = args.extension
+roots = args.roots
 
-for root in roots:
-    for rootdir,dir,files in os.walk(root):
-        if 
-        print files
+
+# for root in roots:
+#     for rootdir,dir,files in os.walk(root):
+#         print files
         
