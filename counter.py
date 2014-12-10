ec86f49188e87a4e72b272e09743643bcb17003e
EricSchles <ericschles@gmail.com>
2014-12-10 09:04:40 -0800

    finished basic idea

--- counter.py
+++ counter.py
@@ -3,6 +3,17 @@ import argparse
 
 #fucking awesome docs - https://mkaz.com/2014/07/26/python-argparse-cookbook/
 
+def file_len(fname):
+    i = 0
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    if i != 0:
+        return i + 1
+    else:
+        return i
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('extension',nargs=1)
 parser.add_argument('roots',nargs='*')
@@ -10,14 +21,17 @@ args = parser.parse_args()
 extension = args.extension[0]
 roots = args.roots
 
+
 total_len = 0
 for root in roots:
     for rootdir,dir,files in os.walk(root):
         for file in files:
             if file.endswith(extension):
-                with open(file,"r") as f:
-                    total_len += len(f)
-
+                file = os.path.join(rootdir,file)
+                print file
+                total_len += file_len(file)
+    
+    print total_len
                 
         
                 
