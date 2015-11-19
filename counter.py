2f60b0adcff6dd5906a968bf46f0c8f12afd90ed
Eric Schles <ericschles@gmail.com>
2015-11-19 17:52:01 -0500

    updating

--- counter.py
+++ counter.py
@@ -20,81 +20,87 @@ class month_total:
         self.ave_hour = ave_hour
         self.num_files = num_files
 
-month_totals = {
-    "01":month_total(),
-    "02":month_total(),
-    "03":month_total(),
-    "04":month_total(),
-    "05":month_total(),
-    "06":month_total(),
-    "07":month_total(),
-    "08":month_total(),
-    "09":month_total(),
-    "10":month_total(),
-    "11":month_total(),
-    "12":month_total()
-}
 
-parser = argparse.ArgumentParser()
-parser.add_argument('extension',nargs=1)
-parser.add_argument('roots',nargs='*')
-args = parser.parse_args()
-extension = args.extension[0]
-roots = args.roots
+def main(extension,roots=[]):
 
-if roots == []:
-    roots.append("~")
+    month_totals = {
+        "01":month_total(),
+        "02":month_total(),
+        "03":month_total(),
+        "04":month_total(),
+        "05":month_total(),
+        "06":month_total(),
+        "07":month_total(),
+        "08":month_total(),
+        "09":month_total(),
+        "10":month_total(),
+        "11":month_total(),
+        "12":month_total()
+    }
 
-total_len = 0
-ave_hour = 0
-num_files = 0
 
-for root in roots:
-    for rootdir,dir,files in os.walk(root):
-        for file in files:
-            if file.endswith(extension) and not file.startswith("."):
-                file = os.path.join(rootdir,file)
-                month = datetime.datetime.fromtimestamp(os.stat(file).st_ctime).strftime("%m")
-                hour = int(datetime.datetime.fromtimestamp(os.stat(file).st_ctime).strftime("%H"))
 
-                month_totals[month].num_files += 1 #per month
-                num_files += 1 #total
+    if roots == []:
+        roots.append("~")
 
-                month_totals[month].ave_hour += hour #per month
-                ave_hour += hour #total
+    total_len = 0
+    ave_hour = 0
+    num_files = 0
 
-                month_totals[month].total_len += file_len(file) #per month
-                total_len += file_len(file) #total
+    for root in roots:
+        for rootdir,dir,files in os.walk(root):
+            for file in files:
+                if file.endswith(extension) and not file.startswith("."):
+                    file = os.path.join(rootdir,file)
+                    month = datetime.datetime.fromtimestamp(os.stat(file).st_ctime).strftime("%m")
+                    hour = int(datetime.datetime.fromtimestamp(os.stat(file).st_ctime).strftime("%H"))
 
-print "You worked best between %s and %s so far" % (str(ave_hour/num_files),str((ave_hour/num_files) + 1))
-print "You've written %d lines so far." % total_len
-print 
+                    month_totals[month].num_files += 1 #per month
+                    num_files += 1 #total
 
-month_translate = {
-    "01":"january",
-    "02":"february",
-    "03":"march",
-    "04":"april",
-    "05":"may",
-    "06":"june",
-    "07":"july",
-    "08":"august",
-    "09":"september",
-    "10":"october",
-    "11":"november",
-    "12":"december"
-}
- 
-            
-for month in month_totals:
-    print "For",month_translate[month]
-    if month_totals[month].num_files != 0:
-        month_totals[month].ave_hour = month_totals[month].ave_hour/month_totals[month].num_files
-    else:
-        month_totals[month].ave_hour = 0
-    print "You worked best between %s and %s so far" % (str(month_totals[month].ave_hour),str(month_totals[month].ave_hour + 1))
-    print "You've written %d lines so far." % (month_totals[month].total_len)
+                    month_totals[month].ave_hour += hour #per month
+                    ave_hour += hour #total
+
+                    month_totals[month].total_len += file_len(file) #per month
+                    total_len += file_len(file) #total
+
+    print "You worked best between %s and %s so far" % (str(ave_hour/num_files),str((ave_hour/num_files) + 1))
+    print "You've written %d lines so far." % total_len
     print 
+
+    month_translate = {
+        "01":"january",
+        "02":"february",
+        "03":"march",
+        "04":"april",
+        "05":"may",
+        "06":"june",
+        "07":"july",
+        "08":"august",
+        "09":"september",
+        "10":"october",
+        "11":"november",
+        "12":"december"
+    }
+
+
+    for month in month_totals:
+        print "For",month_translate[month]
+        if month_totals[month].num_files != 0:
+            month_totals[month].ave_hour = month_totals[month].ave_hour/month_totals[month].num_files
+        else:
+            month_totals[month].ave_hour = 0
+        print "You worked best between %s and %s so far" % (str(month_totals[month].ave_hour),str(month_totals[month].ave_hour + 1))
+        print "You've written %d lines so far." % (month_totals[month].total_len)
+        print 
 
 
                 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('extension',nargs=1)
+    parser.add_argument('roots',nargs='*')
+    args = parser.parse_args()
+    extension = args.extension[0]
+    roots = args.roots
+    main(extension,roots=roots)
